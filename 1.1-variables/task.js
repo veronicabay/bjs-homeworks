e9e98d4878f5cab78f61b1dad15ded6bc0de100e
Veronika Baidina <veronica.bay@mail.ru>
2020-06-08 03:35:03 +0300

    first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,24 @@
  */
 
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = 5;
+	let geography = 3;
+	let physics = 5;
+	let numberOfItems = 3;
+    let averageSum = (algebra + geography + physics) / numberOfItems;
+    return averageSum;
 }
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = 'Veronika';
+    let message = `Привет, мир! Меня зовут ${myName}`;
+	return message;
 }
 
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+    let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;
+    return result;
 }

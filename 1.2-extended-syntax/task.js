33156f35df05ee1ae7d78d9eb99a9d15967f2353
Veronika Baidina <veronica.bay@mail.ru>
2020-06-14 01:20:40 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,36 @@
+"use strict";
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let D = b ** 2 - 4 * a * c;
+    let result = [];
+    if (D < 0) {
+        result = [];
+    } else if (D == 0) {
+        result = [- b / (2 * a)];
+    } else {
+        result [0] = (- b + Math.sqrt(D)) / (2 * a);
+        result [1] = (- b - Math.sqrt(D)) / (2 * a);
+    } return result;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let sum = 0;
+    let averageMark = sum / marks.lengh;
+    if (marks.lengh > 5) {
+      marks.slice(0, 5);
+      console.log("Количество введённых оценок превышает допустимое значение");
+    } for (let i = 0; i <= marks.lengh - 1; i++) {
+        sum = sum + marks[i];
+    } return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let year = new Date().getFullYear();
+    let age = year - dateOfBirthday.getFullYear();
+    let resolution;
+    if (age >= 18) {
+        resolution = "Не желаете ли олд-фэшн, " + name + " ?";
+    } else {
+        resolution = "Сожалею, " + name + " , но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!";
+    }
+    return resolution;
 }

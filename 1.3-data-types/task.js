b079af46ce4094bcbf7da30d2ef464df7af6e781
Veronika Baidina <veronica.bay@mail.ru>
2020-06-16 03:23:25 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,27 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let percentNumber = parseFloat(percent);
+    let contributionNumber = parseFloat(contribution);
+    let amountNumber = parseFloat(amount);
+    if (isNaN(percentNumber)) {
+        return `Параметр 'Процентная ставка' содержит неправильное значение "${percent}"`;
+    } else if (isNaN(contributionNumber)) {
+        return `Параметр 'Сумма первоначального взноса' содержит неправильное значение "${contribution}"`; 
+    } else if (isNaN(amountNumber)) {
+        return `Параметр 'Сумма кредита' содержит неправильное значение "${amount}"`;
+    } else {
+        let creditBody = amount - contribution;
+        let P = percentNumber / 12 / 100;
+        let currentDate = new Date();
+        let n = (date.getFullYear() - currentDate.getFullYear()) * 12 + date.getMonth() - currentDate.getMonth();
+        let monthPay = creditBody * (P + P / (((1 + P)**n) - 1));
+        let totalAmount = monthPay * n;
+        console.log(totalAmount);
+        return parseFloat(totalAmount.toFixed(2));
+    }
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (typeof name !== "string" || name == '') name = "Аноним";
+    return `Привет, мир! Меня зовут ${name}`;
 }
